79f6d848ed28c8685ff445dada7ba7ee31ed6401
Atif Haque <61943868+atifih@users.noreply.github.com>
2020-12-02 03:44:35 +1100

    Skeleton code

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+-- Drops the burgers_db if it already exists --
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Created the DB "burgers_db" (only works on local connections)
+CREATE DATABASE burgers_db;
+
+-- Use the DB burgers_db for all the rest of the script
+USE burgers_db;
+
+-- Created the table "burgers"
+CREATE TABLE burgers (
+  id int AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(256) NOT NULL,
+  devoured BOOLEAN  DEFAULT false,
+  PRIMARY KEY(id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+- Inserted a set of records into the table
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("La Big Beefy", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Da Chicken", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Das Cheezy", false);
