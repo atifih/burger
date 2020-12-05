3938ab4780a6ce160bfed27537632b956d5d542b
Atif Haque <61943868+atifih@users.noreply.github.com>
2020-12-05 11:56:21 +1100

    app.use() requires a middleware function error received

--- config/connection.js
+++ config/connection.js
@@ -2,21 +2,21 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "Rayyan02",
+	database: "burgers_db",
 });
 
 // Make connection.ls
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+	if (err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 // Dependencies
 var express = require("express");
 var exphbs = require("express-handlebars");
-
+var routes = require("./controllers/burgers_controller.js");
 // Create an instance of the express app.
 var app = express();
+// to serve images, CSS files, and JavaScript files in a directory named public using middleware function in Express.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -13,33 +19,10 @@ var PORT = process.env.PORT || 8080;
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Data
-var lunches = [
-  {
-    lunch: "Beet & Goat Cheese Salad with minestrone soup."
-  }, {
-    lunch: "Pizza, two double veggie burgers, fries with a Big Gulp"
-  }
-];
-
-// Routes
-app.get("/weekday", function(req, res) {
-  res.render("index", lunches[0]);
-});
-
-app.get("/weekend", function(req, res) {
-  res.render("index", lunches[1]);
-});
-
-app.get("/lunches", function(req, res) {
-  res.render("all-lunches", {
-    foods: lunches,
-    eater: "david"
-  });
-});
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function () {
+	// Log (server-side) when our server has started
+	console.log("Server listening on: http://localhost:" + PORT);
 });
