f8efabc14c593859c42e83b1c7e9fc73e2158abd
Atif Haque <61943868+atifih@users.noreply.github.com>
2020-12-09 11:48:13 +1100

    Added support for connection to JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,15 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
+const connection;
 
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL{
+	onst connection = mysql.createConnection({
 	host: "localhost",
-	port: 3306,
 	user: "root",
 	password: "Rayyan02",
 	database: "burgers_db",
 });
+})
 
-// Make connection.ls
-
-connection.connect(function (err) {
-	if (err) {
-		console.error("error connecting: " + err.stack);
-		return;
-	}
-	console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
+connection.connect();
 module.exports = connection;
